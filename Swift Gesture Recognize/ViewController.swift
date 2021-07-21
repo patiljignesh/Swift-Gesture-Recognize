d131b5315ef37868c3be5c27063c9947468db2fe
Jignesh Patil <patiljignesh@gmail.com>
2021-07-21 20:51:49 +0200

    Initial Commit

---
+++ Swift Gesture Recognize/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift Gesture Recognize
+//
+//  Created by Jigneshkumar Patil on 2021/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
