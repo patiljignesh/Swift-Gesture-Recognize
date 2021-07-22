2f065c689e69bb65131d1ecb260d7a7c31d1548e
Jignesh Patil <patiljignesh@gmail.com>
2021-07-22 16:37:19 +0200

    Add HandlePan and PanGesture to enable ImageView movement.

--- Swift Gesture Recognize/ViewController.swift
+++ Swift Gesture Recognize/ViewController.swift
@@ -9,11 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var fileStrawberriesImageView: UIImageView!
+    @IBOutlet weak var fileBlueberriesImageView: UIImageView!
+    
+    var fileViewStraberriesOrigin: CGPoint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        addPanGesture(view: fileBlueberriesImageView)
+        fileViewStraberriesOrigin = fileStrawberriesImageView.frame.origin
     }
 
 
+    func addPanGesture(view: UIView) {
+        let pan = UIPanGestureRecognizer(target: self, action: #selector(handlePan(sender:)))
+        view.addGestureRecognizer(pan)
+    }
+    
+   @objc func handlePan(sender: UIPanGestureRecognizer){
+        let fileViewStraberries = sender.view!
+        let translation = sender.translation(in: view)
+        
+    switch sender.state {
+    case .began, .changed:
+        fileViewStraberries.center = CGPoint(x: fileViewStraberries.center.x + translation.x, y: fileViewStraberries.center.y + translation.y)
+        sender.setTranslation(CGPoint.zero, in: view)
+    case .ended:
+        break
+    default:
+        break
+    }
+   }
 }
 
